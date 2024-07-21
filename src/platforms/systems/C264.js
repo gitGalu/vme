30955fe152d02aabdfd513d442fe918b1c8f6aa2
gitGalu <galu83@gmail.com>
2024-07-21 16:27:26 +0200

    add C264 touch controls

--- src/platforms/systems/C264.js
+++ src/platforms/systems/C264.js
@@ -15,12 +15,48 @@ const C264 = {
         '--cursorwidth': '1em',
     },
     shader: ['assets/shaders/crt/crt-geom.glslp', 'assets/shaders/crt/shaders/crt-geom.glsl'],
+    touch_controller_mapping: {
+        input_player1_up: 'F13',
+        input_player1_left: 'F14',
+        input_player1_down: 'F15',
+        input_player1_right: 'F11',
+        input_player1_b: 'kp_minus',
+        input_player1_a: 'nul',
+        input_player1_y: 'nul',
+        input_player1_x: 'nul',
+        input_player1_start: 'nul',
+        input_player1_select: 'nul',
+        input_player1_l: 'nul',
+        input_player1_r: 'nul'
+    },
     fire_buttons: 1,
+    keyboard: {
+        shiftKey: 1,
+        overrides: {
+        }
+    },
     additional_buttons: {
-        // 1: {
-        //     "label": "KB",
-        //     "keyCode": 'select'
-        // }
+        1: {
+            "label": "SPACE",
+            "key": {
+                "key": 'Space',
+                "code": 'Space'
+            }
+        },
+        2: {
+            "label": "RUN/STOP",
+            "key": {
+                "key": 'Escape',
+                "code": 'Escape'
+            }
+        },
+        3: {
+            "label": "F1",
+            "key": {
+                "key": 'F1',
+                "code": 'F1'
+            }
+        }
     }
 };
 
