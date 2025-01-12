d4a64e04c8a3a4627a87b82cd219036c4f7cf94e
gitGalu <galu83@gmail.com>
2025-01-12 19:26:20 +0100

    add GameFocusManager class to manage libretro Game Focus Mode

---
+++ src/keyboard/GameFocusManager.js
@@ -0,0 +1,51 @@
+class GameFocusManager {
+    static #instance = null;
+    #isGameFocusOn;
+    #nostalgist;
+  
+    constructor(nostalgist, initialState = false) {
+      if (GameFocusManager.#instance) {
+        throw new Error('Assertion error.');
+      }
+      this.#nostalgist = nostalgist;
+      this.#isGameFocusOn = initialState;
+      GameFocusManager.#instance = this;
+    }
+  
+    static initialize(nostalgist, initialState = false) {
+      if (!GameFocusManager.#instance) {
+        GameFocusManager.#instance = new GameFocusManager(nostalgist, initialState);
+      }
+      return GameFocusManager.#instance;
+    }
+  
+    static getInstance() {
+      if (!GameFocusManager.#instance) {
+        throw new Error('Assertion error.');
+      }
+      return GameFocusManager.#instance;
+    }
+  
+    #toggle() {
+      this.#isGameFocusOn = !this.#isGameFocusOn;
+      this.#sendCommand();
+    }
+  
+    enable() {
+      if (!this.#isGameFocusOn) {
+        this.#toggle();
+      }
+    }
+  
+    disable() {
+      if (this.#isGameFocusOn) {
+        this.#toggle();
+      }
+    }
+  
+    #sendCommand() {
+      this.#nostalgist.sendCommand('GAME_FOCUS_TOGGLE');
+    }
+  }
+  
+  export default GameFocusManager;
