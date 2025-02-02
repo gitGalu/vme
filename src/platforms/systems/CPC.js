4856f20cde74e0e20d72b323d4256685939a24ce
gitGalu <galu83@gmail.com>
2025-02-02 20:20:44 +0100

    add CPC arrow keys emulation on touchscreens, fix some touchscreen keyboard mappings

--- src/platforms/systems/CPC.js
+++ src/platforms/systems/CPC.js
@@ -32,6 +32,12 @@ const CPC = {
         "Z": "Joystick Fire",
         "X": "Select file on launch"
     },
+    arrow_keys: {
+        up: { key: 'ArrowUp', code: 'ArrowUp', keyCode: 38 },
+        down: { key: 'ArrowDown', code: 'ArrowDown', keyCode: 40 },
+        left: { key: 'ArrowLeft', code: 'ArrowLeft', keyCode: 37 },
+        right: { key: 'ArrowRight', code: 'ArrowRight', keyCode: 39 }
+    },
     touch_controllers: [
         JOYSTICK_TOUCH_MODE.QUICKJOY_PRIMARY,
         JOYSTICK_TOUCH_MODE.QUICKSHOT_DYNAMIC,
@@ -47,9 +53,83 @@ const CPC = {
     },
     fire_buttons: 1,
     keyboard: {
-        shiftKey: 2,
+        shiftKey: 1,
         overrides: {
         }
+    },
+    touch_keyboard_reconfig: {
+        keyMappings: {
+            'keyQ': {
+                value: 'a',
+                code: 'KeyA'
+            },
+            'keyW': {
+                value: 'z',
+                code: 'KeyZ'
+            },
+            'keyA': {
+                value: 'q',
+                code: 'KeyQ'
+            },
+            'keyZ': {
+                value: 'w',
+                code: 'KeyW'
+            },
+            'keyM': {
+                value: ';',
+                code: 'Semicolon'
+            },
+            'key1': {
+                value: '1',
+                code: 'Digit1',
+                shift: true
+            },
+            // 'key2': { 
+            //     value: '2',
+            //     code: 'Digit2',
+            //     shift: true
+            // },
+            // 'key3': {
+            //     value: '3',
+            //     code: 'Digit3',
+            //     shift: true
+            // },
+            'key4': {
+                value: '4',
+                code: 'Digit4',
+                shift: true
+            },
+            'key5': {
+                value: '5',
+                code: 'Digit5',
+                shift: true
+            },
+            // 'key6': {
+            //     value: '6',
+            //     code: 'Digit6',
+            //     shift: true
+            // },
+            // 'key7': {
+            //     value: '7',
+            //     code: 'Digit7',
+            //     shift: true
+            // },
+            'key8': {
+                value: '8',
+                code: 'Digit8',
+                shift: true
+            },
+            'key9': {
+                value: '9',
+                code: 'Digit9',
+                shift: true
+            },
+            'key0': {
+                value: '0',
+                code: 'Digit0',
+                shift: true
+            }
+        },
     },
     additional_buttons: {
     }
